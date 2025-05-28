2381d1dd1aa2bc28a036603e8dab1cf3232f2dea
Adhil Ansari P V <frkadhilansaripv@gmail.com>
2025-05-28 21:38:12 +0530

    deploy test

--- src/app/dashboard/components/Footer.tsx
+++ src/app/dashboard/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
-      <p className="text-sm">© 2025 CoinVerse. All rights reserved.</p>
+      <p className="text-sm">© 2025 CoinVerse. All rights reserved..</p>
     </footer>
   );
 }
